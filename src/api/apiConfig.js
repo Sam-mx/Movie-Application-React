43d639219274664894d639d2be1c8f6d20904439
San Shwe <sanxhwe@gmail.com>
2024-03-17 08:35:33 +0630

    api setup

--- src/api/apiConfig.js
+++ src/api/apiConfig.js
@@ -1,6 +1,6 @@
 const apiConfig = {
     baseUrl: 'https://api.themoviedb.org/3/',
-    apiKey: 'get from themoviedb',
+    apiKey: 'f485694a7564adde0d932371919c61de',
     originalImage: (imgPath) => `https://image.tmdb.org/t/p/original/${imgPath}`,
     w500Image: (imgPath) => `https://image.tmdb.org/t/p/w500/${imgPath}`
 }
