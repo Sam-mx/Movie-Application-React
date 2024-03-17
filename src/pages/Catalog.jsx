12bde9d87dca48168335ada2eb8f886d8c6bfe1f
San Shwe <sanxhwe@gmail.com>
2024-03-17 08:11:44 +0630

    Catalog Page

--- src/pages/Catalog.jsx
+++ src/pages/Catalog.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import React from 'react';
+
+import { useParams } from 'react-router';
+
+import PageHeader from '../components/page-header/PageHeader';
+
+import { category as cate } from '../api/tmdbApi';
+import MovieGrid from '../components/movie-grid/MovieGrid';
 
 const Catalog = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+
+    const { category } = useParams();
+
+    return (
+        <>
+            <PageHeader>
+                {category === cate.movie ? 'Movies' : 'TV Series'}
+            </PageHeader>
+            <div className="container">
+                <div className="section mb-3">
+                    <MovieGrid category={category}/>
+                </div>
+            </div>
+        </>
+    );
 }
 
-export default Catalog
+export default Catalog;
